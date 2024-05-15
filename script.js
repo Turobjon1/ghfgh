d243e0710f8883f159973d90502379c213734e08
Turobjon <avezovzamir337@gmail.com>
2024-05-15 15:13:35 +0500

    hgfhg

---
+++ script.js
@@ -0,0 +1,40 @@
+// let Otadiv = document.getElementById('ota')
+// let body = document.querySelector('body')
+
+// let prt = prompt(' ism kiriting')
+// let prt2 = prompt(' familya kiriting')
+
+// let h1 = document.createElement('h1')
+// h1.innerHTML = ' Ism: ' + prt + ' Familya: ' + prt2 
+
+// h1.style.display = 'flex'
+// h1.style.justifyContent = 'center'
+// h1.style.alignItems = 'center'
+// h1.style.height = '100vh'
+// h1.style.color = 'green'
+// body.style.backgroundColor = 'black'
+
+
+
+// let div1 = document.getElementById('ota')
+
+
+// Otadiv.appendChild(h1)
+let body = document.querySelector('body')
+let lorem = document.getElementById('lorem')
+
+
+let prt = prompt('enter ekran backgroundColor')
+ let prt2 = prompt('enter text color')
+
+
+  body.style.backgroundColor = prt
+  lorem.style.color = prt2
+
+
+
+
+
+
+
+lorem.style
